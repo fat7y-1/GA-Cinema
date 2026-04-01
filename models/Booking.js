38fde481dcd33ee0a03a6a22a3097b05cc52ce7d
fathy soliman <fathi123soliman@gmail.com>
2026-04-01 10:56:35 +0300

    update models

--- models/Booking.js
+++ models/Booking.js
@@ -3,14 +3,21 @@ const mongoose = require("mongoose")
 const bookingSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    description: { type: String, required: true },
     language: { type: String, require: true },
     picture: { type: String, require: true },
+    userTicket: { type: Number, require: true },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
     },
+    movieId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Movie",
+      required: true,
+    },
+    selectedSeats: { type: [Number], default: [] },
+    isDone: { type: Boolean },
   },
   { timestamps: true }
 )
